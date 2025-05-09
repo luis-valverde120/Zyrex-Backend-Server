0d960ab5fbf46b0e96c7fb70fd86786395c38e69
luis-valverde120 <luis.valverde@upec.edu.ec>
2025-05-08 23:03:31 -0500

    feat: Add methods from product models

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/luis-valverde120/Zyrex-Backend-Server
 
-go 1.21.1
+go 1.23.0
+
+toolchain go1.23.9
 
 require (
 	github.com/bytedance/sonic v1.11.6 // indirect
@@ -24,10 +26,10 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/core/models/product.go
+++ internal/core/models/product.go
@@ -1,5 +1,7 @@
 package models
 
+import "errors"
+
 /*
 * Product Represents a product in the e-commerce application.
 * It contains fields for the product ID, name, description, image URL, price, stock, size, weight,
@@ -17,4 +19,75 @@ type Product struct {
 	Color       string  `json:"color"`
 	StoreID     int     `json:"store_id"`
 	CategoryID  int     `json:"category_id"`
+}
+
+// methods
+// ChangePrice updates the price of the product. If the new price is negative, it sets the price to 0 and returns an error.
+func (p *Product) ChangePrice(price float64) error {
+	if price < 0 {
+		price = 0
+		//return error  the price not negative
+		return errors.New("price cannot be negative")
+	}
+	p.Price = price
+	return nil
+}
+
+// ChangeStock update the stock of the product. If the new stock is negative, it sets the stock to 0 and returns an error.
+func (p *Product) ChangeStock(stock int) error {
+	if stock < 0 {
+		stock = 0
+		//return error  the stock not negative
+		return errors.New("stock cannot be negative")
+	}
+	p.Stock = stock
+	return nil
+}
+
+// DecreaseStock decreases the stock of the product by the specified quantity. If the quantity is negative or greater than the current stock, it returns an error.
+func (p *Product) DecreaseStock(quantity int) error {
+	// Validate if has a stock of procut
+	if quantity < 0 {
+		return errors.New("quantity cannot be negative")
+	}
+	if p.Stock < quantity {
+		return errors.New("not enough stock")
+	}
+	p.Stock -= quantity
+	return nil
+}
+
+func (p *Product) Validate() error {
+	if p.Name == "" {
+		return errors.New("name is required")
+	}
+	if p.Description == "" {
+		return errors.New("description is required")
+	}
+	if p.ImgURL == "" {
+		return errors.New("image URL is required")
+	}
+	if p.Price <= 0 {
+		return errors.New("price must be greater than 0")
+	}
+	if p.Stock < 0 {
+		return errors.New("stock cannot be negative")
+	}
+
+	/*
+	* Size, Weight, Color are optional fields, so we don't need to validate them.
+	 */
+
+	if p.StoreID <= 0 {
+		return errors.New("store ID must be greater than 0")
+	}
+	if p.CategoryID <= 0 {
+		return errors.New("category ID must be greater than 0")
+	}
+	return nil
+}
+
+// GetProduct returns the product details.
+func (p *Product) GetProduct() Product {
+	return *p
 }
