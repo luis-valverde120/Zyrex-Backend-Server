dcf5297d13f93828d91e9856616d476dc1d4d476
luis-valverde120 <luis.valverde@upec.edu.ec>
2025-05-08 23:15:35 -0500

    feat: Add methods from cart_item models

--- internal/core/models/cart_item.go
+++ internal/core/models/cart_item.go
@@ -1,5 +1,7 @@
 package models
 
+import "errors"
+
 /*
 * CartItem Represents an item in a shopping cart.
 * It contains fields for the cart ID, product ID, and quantity of the item.
@@ -9,4 +11,29 @@ type CartItem struct {
 	CartID    int `json:"cart_id"`
 	ProductID int `json:"product_id"`
 	Quantity  int `json:"quantity"`
+}
+
+// * Methods for CartItem struct */
+
+// GetCartID returns the cart ID of the CartItem.
+func (cart_item *CartItem) GetID() int {
+	return cart_item.ID
+}
+
+// GetCartID returns the cart ID of the CartItem.
+func (cart_item *CartItem) GetCartID() int {
+	return cart_item.CartID
+}
+
+func (cart_item *CartItem) ValidateAddItem() error {
+	if cart_item.CartID == 0 {
+		return errors.New("cart ID is required")
+	}
+	if cart_item.ProductID == 0 {
+		return errors.New("product ID is required")
+	}
+	if cart_item.Quantity <= 0 {
+		return errors.New("quantity must be greater than 0")
+	}
+	return nil
 }
