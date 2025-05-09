855bf4a7fae32bfdac25dc37c540c132fd537239
luis-valverde120 <luis.valverde@upec.edu.ec>
2025-05-09 00:03:00 -0500

    feat: Add method validate in category model

--- internal/core/models/category.go
+++ internal/core/models/category.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"strings"
+)
+
 /*
 * Category Represents a product category in the e-commerce application.
 * It contains fields for the category ID, name, and description.
@@ -8,4 +13,19 @@ type Category struct {
 	ID          int    `json:"id" gorm:"primaryKey"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
+}
+
+// * methods for Category struct can be added here if needed
+
+// Validate checks if the category name is valid.
+func (cart *Category) Validate() error {
+	if cart.Name == "" {
+		return errors.New("category name is required")
+	}
+
+	if strings.TrimSpace(cart.Name) == "" {
+		return errors.New("category name cannot be empty or whitespace")
+	}
+
+	return nil
 }
