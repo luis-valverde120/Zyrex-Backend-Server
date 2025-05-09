94397f0efcc053c1edac741e669355a6f4cd039e
luis-valverde120 <luis.valverde@upec.edu.ec>
2025-05-09 08:19:51 -0500

    feat: ADd methods from client model

--- internal/core/models/city.go
+++ internal/core/models/city.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"strings"
+)
+
 /*
 * City Represents a city in the e-commerce application.
 * It contains fields for the city ID and name.
@@ -7,4 +12,11 @@ package models
 type City struct {
 	ID   int    `json:"id" gorm:"primaryKey"`
 	Name string `json:"name"`
+}
+
+func (city *City) Validate() error {
+	if strings.TrimSpace(city.Name) == "" {
+		return errors.New("city name is required")
+	}
+	return nil
 }
--- internal/core/models/client.go
+++ internal/core/models/client.go
@@ -1,5 +1,13 @@
 package models
 
+import (
+	"errors"
+	"net/mail"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 /*
 * Client Represents a client in the e-commerce application.
 * It contains fields for the client ID, first name, last name, phone number, email
@@ -16,4 +24,85 @@ type Client struct {
 	Gender    string `json:"gender"`
 	Age       int    `json:"age"`
 	AddressID int    `json:"address_id"`
+}
+
+// * methods for Client struct
+
+// Valiidate checks if the Client has all the required fields to be created or updated.
+func (client *Client) Validate() error {
+	if strings.TrimSpace(client.FirstName) == "" {
+		return errors.New("first name is required")
+	}
+	if strings.TrimSpace(client.LastName) == "" {
+		return errors.New("last name is required")
+	}
+	if err := client.IsValidPhone(client.Phone); err != nil {
+		return err
+	}
+	if strings.TrimSpace(client.Email) == "" {
+		return errors.New("email address is required")
+	}
+	if strings.TrimSpace(client.Password) == "" {
+		return errors.New("password is required")
+	}
+	if client.Age <= 0 {
+		return errors.New("age must be greater than 0")
+	}
+
+	// validate if gender is male or female
+	if !client.IsValidGender(client.Gender) {
+		return errors.New("gender must be male, female, or other")
+	}
+
+	return nil
+}
+
+// IsValidGender is a validator of the gender field.
+func (client *Client) IsValidGender(gender string) bool {
+	switch strings.ToLower(gender) {
+	case "male", "female", "other":
+		return true
+	}
+	return false
+}
+
+// IsValidPhone checks if the phone number is valid.
+func (client *Client) IsValidPhone(phone string) error {
+	if len(phone) <= 8 {
+		return errors.New("phone number must be at least 8 characters long")
+	}
+	return nil
+}
+
+// IsValidEmail checks if the email address is valid.
+func (client *Client) IsValidEmail(email string) error {
+	if strings.TrimSpace(email) == "" {
+		return errors.New("email address is required")
+	}
+
+	if _, err := mail.ParseAddress(email); err != nil {
+		return errors.New("invalid email address")
+	}
+
+	return nil
+}
+
+// VerifyPassword checks if the password is valid.
+func (client *Client) VerifyPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(client.Password), []byte(password))
+	return err == nil
+}
+
+// HashPassword hashes the password using bcrypt.
+func (client *Client) HashPassword(password string) error {
+	hashed_password, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	client.Password = string(hashed_password)
+	return nil
+}
+
+func (client *Client) FullName() string {
+	return client.FirstName + " " + client.LastName
 }
